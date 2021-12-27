845408b6d1c6e2b5bd552f89c783918db677beaf
Brian Armstrong <brian.armstrong@discordapp.com>
2021-12-27 00:23:04 -0800

    state change

--- index.md
+++ index.md
@@ -11,8 +11,8 @@ Convert volume slider fractions to amplitudes and nothing more.
     <input type="range" style="vertical-align: top;margin-left: 5px;margin-right: 5px;width: 252px;" id="input-volume-1">
     <i class="fa fa-volume-up" style="font-size: 24px;"></i>
   </div>
-  <div id="label-slider-1">Slider position: 50%</div>
-  <div id="label-amplitude-1">Amplitude: 50%</div>
+  <div id="label-slider-1" hidden=true>Slider position: 50%</div>
+  <div id="label-amplitude-1" hidden=true>Amplitude: 50%</div>
 
 ## Perceptual
   Now we will demonstrate again using Perceptual's amplitude conversion.
@@ -23,5 +23,5 @@ Convert volume slider fractions to amplitudes and nothing more.
     <input type="range" style="vertical-align: top;margin-left: 5px;margin-right: 5px;width: 252px;" id="input-volume-2">
     <i class="fa fa-volume-up" style="font-size: 24px;"></i>
   </div>
-  <div id="label-slider-2">Slider position: 50%</div>
-  <div id="label-amplitude-2">Amplitude: 50%</div>
+  <div id="label-slider-2" hidden=true>Slider position: 50%</div>
+  <div id="label-amplitude-2" hidden=true>Amplitude: 50%</div>
--- index.ts
+++ index.ts
@@ -15,60 +15,90 @@ import { perceptualToAmplitude } from '@discordapp/perceptual';
     gainNode.gain.value = 0.5;
     oscillatorNode.start();
 
-    const playButton1 = document.getElementById('play-button-1');
-    const playButton2 = document.getElementById('play-button-2');
+    const playButton1 = document.getElementById('play-button-1')!;
+    const playButton2 = document.getElementById('play-button-2')!;
 
-    const volume1 = document.getElementById('input-volume-1');
-    const volume2 = document.getElementById('input-volume-2');
+    const volume1 = document.getElementById('input-volume-1')!;
+    const volume2 = document.getElementById('input-volume-2')!;
 
-    const labelSlider1 = document.getElementById('label-slider-1');
-    const labelSlider2 = document.getElementById('label-slider-2');
+    const labelSlider1 = document.getElementById('label-slider-1')!;
+    const labelSlider2 = document.getElementById('label-slider-2')!;
 
-    const labelAmplitude1 = document.getElementById('label-amplitude-1');
-    const labelAmplitude2 = document.getElementById('label-amplitude-2');
+    const labelAmplitude1 = document.getElementById('label-amplitude-1')!;
+    const labelAmplitude2 = document.getElementById('label-amplitude-2')!;
 
     let state = 'stopped';
 
-    playButton1!.addEventListener('click', () => {
+    playButton1.addEventListener('click', () => {
         audioCtx.resume();
         if (state === 'stopped') {
             gainNode.connect(audioCtx.destination);
             state = 'running-1';
+            playButton1.innerText = 'Stop Tone';
+            playButton2.innerText = 'Start Tone';
+            labelSlider1.hidden = false;
+            labelAmplitude1.hidden = false;
+            labelSlider2.hidden = true;
+            labelAmplitude2.hidden = true;
         } else if (state === 'running-2') {
             state = 'running-1';
+            playButton1.innerText = 'Stop Tone';
+            playButton2.innerText = 'Start Tone';
+            labelSlider1.hidden = false;
+            labelAmplitude1.hidden = false;
+            labelSlider2.hidden = true;
+            labelAmplitude2.hidden = true;
         } else {
             gainNode.disconnect(audioCtx.destination);
             state = 'stopped';
+            playButton1.innerText = 'Start Tone';
+            labelSlider1.hidden = true;
+            labelAmplitude1.hidden = true;
         }
     }, false);
 
-    playButton2!.addEventListener('click', () => {
+    playButton2.addEventListener('click', () => {
         audioCtx.resume();
         if (state === 'stopped') {
             gainNode.connect(audioCtx.destination);
             state = 'running-2';
+            playButton1.innerText = 'Start Tone';
+            playButton2.innerText = 'Stop Tone';
+            labelSlider1.hidden = true;
+            labelAmplitude1.hidden = true;
+            labelSlider2.hidden = false;
+            labelAmplitude2.hidden = false;
         } else if (state === 'running-1') {
             state = 'running-2';
+            playButton1.innerText = 'Start Tone';
+            playButton2.innerText = 'Stop Tone';
+            labelSlider1.hidden = true;
+            labelAmplitude1.hidden = true;
+            labelSlider2.hidden = false;
+            labelAmplitude2.hidden = false;
         } else {
             gainNode.disconnect(audioCtx.destination);
             state = 'stopped';
+            playButton2.innerText = 'Start Tone';
+            labelSlider2.hidden = true;
+            labelAmplitude2.hidden = true;
         }
     }, false);
 
-    volume1!.addEventListener('input', () => {
-        const value = (volume1! as HTMLInputElement).valueAsNumber;
-        labelSlider1!.innerText = `Slider Position: ${value}%`;
-        labelAmplitude1!.innerText = `Amplitude: ${value}%`;
+    volume1.addEventListener('input', () => {
+        const value = (volume1 as HTMLInputElement).valueAsNumber;
+        labelSlider1.innerText = `Slider Position: ${value}%`;
+        labelAmplitude1.innerText = `Amplitude: ${value}%`;
         if (state === 'stopped' || state === 'running-1') {
             gainNode.gain.value = value / 100;
         }
     }, false);
 
-    volume2!.addEventListener('input', () => {
-        const value = (volume2! as HTMLInputElement).valueAsNumber;
+    volume2.addEventListener('input', () => {
+        const value = (volume2 as HTMLInputElement).valueAsNumber;
         const amp = perceptualToAmplitude(value, 100);
-        labelSlider2!.innerText = `Slider Position: ${value}%`;
-        labelAmplitude2!.innerText = `Amplitude: ${amp.toFixed(3)}%`;
+        labelSlider2.innerText = `Slider Position: ${value}%`;
+        labelAmplitude2.innerText = `Amplitude: ${amp.toFixed(3)}%`;
         if (state === 'stopped' || state === 'running-2') {
             gainNode.gain.value = amp / 100;
         }
