7768192e1e303b28312256297be3bbc522c21f8a
Brian Armstrong <brian.armstrong@discordapp.com>
2021-12-26 23:39:59 -0800

    more controls

--- index.md
+++ index.md
@@ -1,15 +1,25 @@
-## Direct Conversion
+Have you ever encountered a video or some music that was too loud and moved the volume slider down halfway, only to discover it was still basically as loud as before you moved the slider? The most common approach for developers building these sliders is to take the ratio of how far along the slider moved and apply that precise ratio as a multiplier on the sound's amplitude. Surprisingly, this approach doesn't actually mirror how we perceive loudness. Enter perceptual.
+
+Convert volume slider fractions to amplitudes and nothing more.
+
+## Direct Ratio
+  First we will demonstrate what happens when we do connect the volume slider directly to the gain of the oscillator.
   <button style="width: 300px;" id="play-button-1"><i class="fa fa-play" style="margin-right: 5 px;padding-right: 5px;"></i>Play Tone</button>
   <div style="width: 350px;">
     <i class="fa fa-volume-down" style="/*! height: 100%; */ font-size: 24px;"></i>
     <input type="range" style="vertical-align: top;margin-left: 5px;margin-right: 5px;width: 252px;" id="input-volume-1">
     <i class="fa fa-volume-up" style="font-size: 24px;"></i>
   </div>
+  <div id="label-slider-1">Slider position: 50%</div>
+  <div id="label-amplitude-1">Amplitude: 50%</div>
 
 ## Perceptual
+  Now we will demonstrate again using Perceptual's amplitude conversion.
   <button style="width: 300px;" id="play-button-2"><i class="fa fa-play" style="margin-right: 5 px;padding-right: 5px;"></i>Play Tone</button>
   <div style="width: 350px;">
     <i class="fa fa-volume-down" style="/*! height: 100%; */ font-size: 24px;"></i>
     <input type="range" style="vertical-align: top;margin-left: 5px;margin-right: 5px;width: 252px;" id="input-volume-2">
     <i class="fa fa-volume-up" style="font-size: 24px;"></i>
   </div>
+  <div id="label-slider-2">Slider position: 50%</div>
+  <div id="label-amplitude-2">Amplitude: 50%</div>
--- index.ts
+++ index.ts
@@ -21,13 +21,21 @@ import { perceptualToAmplitude } from '@discordapp/perceptual';
     const volume1 = document.getElementById('input-volume-1');
     const volume2 = document.getElementById('input-volume-2');
 
+    const labelSlider1 = document.getElementById('label-slider-1');
+    const labelSlider2 = document.getElementById('label-slider-2');
+
+    const labelAmplitude1 = document.getElementById('label-amplitude-1');
+    const labelAmplitude2 = document.getElementById('label-amplitude-2');
+
     let state = 'stopped';
 
     playButton1!.addEventListener('click', () => {
         audioCtx.resume();
         if (state === 'stopped') {
             gainNode.connect(audioCtx.destination);
-            state = 'running';
+            state = 'running-1';
+        } else if (state === 'running-2') {
+            state = 'running-1';
         } else {
             gainNode.disconnect(audioCtx.destination);
             state = 'stopped';
@@ -38,7 +46,9 @@ import { perceptualToAmplitude } from '@discordapp/perceptual';
         audioCtx.resume();
         if (state === 'stopped') {
             gainNode.connect(audioCtx.destination);
-            state = 'running';
+            state = 'running-2';
+        } else if (state === 'running-1') {
+            state = 'running-2';
         } else {
             gainNode.disconnect(audioCtx.destination);
             state = 'stopped';
@@ -47,11 +57,20 @@ import { perceptualToAmplitude } from '@discordapp/perceptual';
 
     volume1!.addEventListener('input', () => {
         const value = (volume1! as HTMLInputElement).valueAsNumber;
-        console.log(`${value}`);
+        labelSlider1!.innerText = `Slider Position: ${value}%`;
+        labelAmplitude1!.innerText = `Amplitude: ${value}%`;
+        if (state === 'stopped' || state === 'running-1') {
+            gainNode.gain.value = value / 100;
+        }
     }, false);
 
     volume2!.addEventListener('input', () => {
         const value = (volume2! as HTMLInputElement).valueAsNumber;
-        console.log(`${perceptualToAmplitude(value, 100)}`);
+        const amp = perceptualToAmplitude(value, 100);
+        labelSlider1!.innerText = `Slider Position: ${value}%`;
+        labelAmplitude1!.innerText = `Amplitude: ${amp}%`;
+        if (state === 'stopped' || state === 'running-2') {
+            gainNode.gain.value = amp / 100;
+        }
     }, false);
 })();
