baa57cc26a191a2473f93835408628a5699e3971
Brian Armstrong <brian.armstrong@discordapp.com>
2021-12-28 02:13:27 -0800

    refactor

--- index.ts
+++ index.ts
@@ -1,6 +1,50 @@
 import { perceptualToAmplitude } from '@discordapp/perceptual';
 
+const demoList = ['1', '2'];
 
+interface Demo {
+    [key: string]: HTMLElement;
+}
+
+interface DemoCollection {
+    [key: string]: Demo
+}
+
+function getDemos(): DemoCollection {
+    const demos: DemoCollection = {};
+
+    for (const d of demoList) {
+        demos[d].playButton = document.getElementById(`play-button-${d}`)!;
+        demos[d].volume = document.getElementById(`input-volume-${d}`)!;
+        demos[d].divVolume = document.getElementById(`div-volume-${d}`)!;
+        demos[d].labelPlayButton = document.getElementById(`label-play-button-${d}`)!;
+        demos[d].iconPlayButton = document.getElementById(`icon-play-button-${d}`)!;
+        demos[d].labelSlider = document.getElementById(`label-slider-${d}`)!;
+        demos[d].labelAmplitude = document.getElementById(`label-amplitude-${d}`)!;
+    }
+    return demos;
+}
+
+function showDemo(demos: DemoCollection, identifier: string) {
+    for (const d in demos) {
+        if (d === identifier) {
+            continue;
+        }
+        hideDemo(demos, d);
+    };
+
+    const demo = demos[identifier]!;
+    demo.playButton.innerText = 'Stop Tone';
+    demo.iconPlayButton.classList.replace('fa-play', 'fa-stop');
+    demo.divVolume.hidden = false;
+}
+
+function hideDemo(demos: DemoCollection, identifier: string) {
+    const demo = demos[identifier]!;
+    demo.playButton.innerText = 'Play Tone';
+    demo.iconPlayButton.classList.replace('fa-stop', 'fa-play');
+    demo.divVolume.hidden = true;
+}
 
 (()=> {
     let audioCtx = new AudioContext();
@@ -15,99 +59,56 @@ import { perceptualToAmplitude } from '@discordapp/perceptual';
     gainNode.gain.value = 0.5;
     oscillatorNode.start();
 
-    const playButton1 = document.getElementById('play-button-1')!;
-    const playButton2 = document.getElementById('play-button-2')!;
-
-    const volume1 = document.getElementById('input-volume-1')!;
-    const volume2 = document.getElementById('input-volume-2')!;
-
-    const divVolume1 = document.getElementById('div-volume-1')!;
-    const divVolume2 = document.getElementById('div-volume-2')!;
-
-    const labelPlayButton1 = document.getElementById('label-play-button-1')!;
-    const labelPlayButton2 = document.getElementById('label-play-button-2')!;
-
-    const iconPlayButton1 = document.getElementById('icon-play-button-1')!;
-    const iconPlayButton2 = document.getElementById('icon-play-button-2')!;
-
-    const labelSlider1 = document.getElementById('label-slider-1')!;
-    const labelSlider2 = document.getElementById('label-slider-2')!;
-
-    const labelAmplitude1 = document.getElementById('label-amplitude-1')!;
-    const labelAmplitude2 = document.getElementById('label-amplitude-2')!;
+    const demos = getDemos();
 
     let state = 'stopped';
 
-    playButton1.addEventListener('click', () => {
+    demos[1].playButton.addEventListener('click', () => {
         audioCtx.resume();
         if (state === 'stopped') {
             gainNode.connect(audioCtx.destination);
             state = 'running-1';
-            labelPlayButton1.innerText = 'Stop Tone';
-            labelPlayButton2.innerText = 'Play Tone';
-            iconPlayButton1.classList.replace('fa-play', 'fa-stop');
-            iconPlayButton2.classList.replace('fa-stop', 'fa-play');
-            divVolume1.hidden = false;
-            divVolume2.hidden = true;
+            showDemo(demos, '1');
         } else if (state === 'running-2') {
             state = 'running-1';
-            labelPlayButton1.innerText = 'Stop Tone';
-            labelPlayButton2.innerText = 'Play Tone';
-            iconPlayButton1.classList.replace('fa-play', 'fa-stop');
-            iconPlayButton2.classList.replace('fa-stop', 'fa-play');
-            divVolume1.hidden = false;
-            divVolume2.hidden = true;
+            showDemo(demos, '1');
         } else {
             gainNode.disconnect(audioCtx.destination);
             state = 'stopped';
-            labelPlayButton1.innerText = 'Play Tone';
-            iconPlayButton1.classList.replace('fa-stop', 'fa-play');
-            divVolume1.hidden = true;
+            hideDemo(demos, '1');
         }
     }, false);
 
-    playButton2.addEventListener('click', () => {
+    demos[2].playButton.addEventListener('click', () => {
         audioCtx.resume();
         if (state === 'stopped') {
             gainNode.connect(audioCtx.destination);
             state = 'running-2';
-            labelPlayButton1.innerText = 'Play Tone';
-            labelPlayButton2.innerText = 'Stop Tone';
-            iconPlayButton1.classList.replace('fa-stop', 'fa-play');
-            iconPlayButton2.classList.replace('fa-play', 'fa-stop');
-            divVolume1.hidden = true;
-            divVolume2.hidden = false;
+            showDemo(demos, '2');
         } else if (state === 'running-1') {
             state = 'running-2';
-            labelPlayButton1.innerText = 'Play Tone';
-            labelPlayButton2.innerText = 'Stop Tone';
-            iconPlayButton1.classList.replace('fa-stop', 'fa-play');
-            iconPlayButton2.classList.replace('fa-play', 'fa-stop');
-            divVolume1.hidden = true;
-            divVolume2.hidden = false;
+            showDemo(demos, '2');
         } else {
             gainNode.disconnect(audioCtx.destination);
             state = 'stopped';
-            labelPlayButton2.innerText = 'Play Tone';
-            iconPlayButton2.classList.replace('fa-stop', 'fa-play');
-            divVolume2.hidden = true;
+            hideDemo(demos, '2');
         }
     }, false);
 
-    volume1.addEventListener('input', () => {
-        const value = (volume1 as HTMLInputElement).valueAsNumber;
-        labelSlider1.innerText = `Slider Position: ${value}%`;
-        labelAmplitude1.innerText = `Amplitude: ${value}%`;
+    demos[1].volume.addEventListener('input', () => {
+        const value = (demos[1].volume as HTMLInputElement).valueAsNumber;
+        demos[1].labelSlider.innerText = `Slider Position: ${value}%`;
+        demos[1].labelAmplitude.innerText = `Amplitude: ${value}%`;
         if (state === 'stopped' || state === 'running-1') {
             gainNode.gain.value = value / 100;
         }
     }, false);
 
-    volume2.addEventListener('input', () => {
-        const value = (volume2 as HTMLInputElement).valueAsNumber;
+    demos[2].volume.addEventListener('input', () => {
+        const value = (demos[2].volume as HTMLInputElement).valueAsNumber;
         const amp = perceptualToAmplitude(value, 100);
-        labelSlider2.innerText = `Slider Position: ${value}%`;
-        labelAmplitude2.innerText = `Amplitude: ${amp.toFixed(3)}%`;
+        demos[2].labelSlider.innerText = `Slider Position: ${value}%`;
+        demos[2].labelAmplitude.innerText = `Amplitude: ${amp.toFixed(3)}%`;
         if (state === 'stopped' || state === 'running-2') {
             gainNode.gain.value = amp / 100;
         }
